154fe536bb52c7584c355be64f7bbebb4e407d9e
arthur <arthur.broudoux@gmail.com>
2024-12-07 15:12:32 +0100

    feat: update sha Signed-off-by: Arthur Broudoux <arthur.broudoux@gmail.com>

--- Formula/hello.rb
+++ Formula/hello.rb
@@ -2,7 +2,7 @@ class Hello < Formula
     desc "Homebrew Test Package"
     homepage "https://github.com/abroudoux/hello"
     url "https://github.com/abroudoux/hello/archive/refs/tags/1.1.0.tar.gz"
-    sha256 "c1629222afecc4a60b07b4e0daa914e5af564698"
+    sha256 "40e97baa22bc45b3956425a75906d11533bfb2c167235924402dd14a78a39f98"
     license "MIT"
 
     def install
