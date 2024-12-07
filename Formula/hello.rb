83807007f72085317c93492818f90666099f93f8
arthur <arthur.broudoux@gmail.com>
2024-12-07 14:57:41 +0100

    feat: update hello.rb (wrong sha256) Signed-off-by: Arthur Broudoux <arthur.broudoux@gmail.com>

--- Formula/hello.rb
+++ Formula/hello.rb
@@ -4,7 +4,7 @@ class Hello < Formula
     desc "Homebrew Test Package"
     homepage "https://github.com/abroudoux/hello"
     url "https://github.com/abroudoux/hello/archive/refs/tags/1.1.0.tar.gz"
-    sha256 "d13d9a3ffaeccafa25ddd4d744d69c9b0132b95c5c7881b9aa8231f19e5e82b0"
+    sha256 "c1629222afecc4a60b07b4e0daa914e5af564698"
     license "MIT"
   
     def install
